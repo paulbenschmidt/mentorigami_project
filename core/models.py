be6c43dad522875c86028e31abfe9a398cbdd19d
Paul Schmidt <paulbenschmidt@gmail.com>
2021-02-01 19:00:10 -0600

    create base model inheritance and initialize accounts app

--- core/models.py
+++ core/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+class TimeStampedModel(models.Model):
+    """
+    An abstract base class model that provides self-updating
+    ``created`` and ``modified`` fields.
+    """
+    created = models.DateTimeField(auto_now_add=True)
+    modified = models.DateTimeField(auto_now=True)
+
+    # By defining as abstract base class, Django doesn’t create
+    # core_timestampedmodel table when migrate is run
+    class Meta:
+        abstract = True
